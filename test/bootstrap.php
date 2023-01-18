aee3bdfcfa5ce56697c01bc4d0e193162dba482c
robertkleinschuster <github@kleinschuster.de>
2023-01-18 13:54:05 +0100

    Disable XDebug in GitHub-Workflow

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,6 +4,5 @@ chdir(dirname(__DIR__));
 
 include 'vendor/autoload.php';
 
-ini_set('xdebug.max_nesting_level', 512);
 \Blue\Core\Logger\Logger::$logger = new \Psr\Log\NullLogger();
 \Blue\Core\Database\Connection::$test = true;
