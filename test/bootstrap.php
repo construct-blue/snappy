d4cc10a6925639694e32acca08ed079547c91a79
robertkleinschuster <github@kleinschuster.de>
2023-01-18 13:44:35 +0100

    Navigation improvements and "My Account"-Page

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,5 +4,6 @@ chdir(dirname(__DIR__));
 
 include 'vendor/autoload.php';
 
+ini_set('xdebug.max_nesting_level', 512);
 \Blue\Core\Logger\Logger::$logger = new \Psr\Log\NullLogger();
 \Blue\Core\Database\Connection::$test = true;
