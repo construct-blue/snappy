205e71f9c5f3d38c16d310b6ff202fe443c8f3d4
robertkleinschuster <github@kleinschuster.de>
2023-02-14 22:47:46 +0100

    use form component in user add

--- src/Snapps/System/Settings/User/View/UserAdd.php
+++ src/Snapps/System/Settings/User/View/UserAdd.php
@@ -2,6 +2,7 @@
 
 namespace Blue\Snapps\System\Settings\User\View;
 
+use Blue\Core\View\Component\Form\Form;
 use Blue\Core\View\Component\Form\Textfield;
 use Blue\Core\View\Component\Icon\Icon;
 use Blue\Core\View\ViewComponent;
@@ -11,18 +12,22 @@ class UserAdd extends ViewComponent
     public function render(): array
     {
         return [
-            'form is="reactive-form" method="post" action="{activePath}/add"' => [
-                Textfield::class => [
-                    'name' => 'name',
-                    'placeholder' => 'Username to add',
-                    'required' => true,
-                ],
-                'button type="submit"' => [
-                    Icon::class => [
-                        'icon' => 'plus'
+            Form::class => [
+                'method' => 'post',
+                'action' => '{activePath}/add',
+                'content' =>  [
+                    Textfield::class => [
+                        'name' => 'name',
+                        'placeholder' => 'Username to add',
+                        'required' => true,
                     ],
-                    'span' => 'Add user'
-                ]
+                    'button type="submit"' => [
+                        Icon::class => [
+                            'icon' => 'plus'
+                        ],
+                        'span' => 'Add user'
+                    ]
+                ],
             ],
         ];
     }
