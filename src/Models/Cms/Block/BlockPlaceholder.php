ff648f6465da28a5234c775e087e3f35369dea53
robertkleinschuster <github@kleinschuster.de>
2023-02-14 19:42:19 +0100

    allow streams for block placeholder replace

--- src/Models/Cms/Block/BlockPlaceholder.php
+++ src/Models/Cms/Block/BlockPlaceholder.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace Blue\Models\Cms\Block;
 
+use Laminas\Diactoros\CallbackStream;
+use ParsedownExtra;
+use Psr\Http\Message\StreamInterface;
+
 class BlockPlaceholder
 {
     public function __construct(private readonly BlockRepository $repository)
     {
     }
 
-    public function replace(string $str): string
+    public function replace(string|StreamInterface $str): string|StreamInterface
     {
-        $parsedown = new \ParsedownExtra();
+        if ($str instanceof CallbackStream) {
+            return new CallbackStream(fn() => $this->replace($str->getContents()));
+        }
+        $parsedown = new ParsedownExtra();
         $blocks = $this->repository->findAll();
         foreach ($blocks as $block) {
             $str = str_replace("{block:{$block->getCode()}}", $parsedown->text($block->getContent()), $str);
